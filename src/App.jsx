505f84faf7a0b85b76f70575142df7ca08f65d21
Sabin <you@example.com>
2024-03-25 23:15:16 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Home from './pages/home/Home'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SingleProduct from './pages/singleProduct/SingleProduct'
+import Edit from './pages/editProduct/Edit'
+import AddProduct from './pages/addProduct/AddProduct'
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product/id' element={<SingleProduct/>}/>
+        <Route path='/addproduct' element={<AddProduct/>}/>
+        <Route path='editproduct/id' element={<Edit/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav>
+    <div className="container">
+        <div className="logo">Your Logo</div>
+        <ul className="nav-links">
+            {/* <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li> */}
+        </ul> 
+        <a href="#" class="add-product-btn">Add Product</a>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
